93d8c45dae34231f2c84acb4e8e7084772071979
123seven <42730681+123seven@users.noreply.github.com>
2019-07-09 18:03:16 +0800

    Update template_msg.py

--- wechat/api/template_msg.py
+++ wechat/api/template_msg.py
@@ -74,11 +74,3 @@ class WeChatTemplateMessage(BaseWeChatAPI):
 
 
 __all__ = ['WeChatTemplateMessage']
-
-if __name__ == '__main__':
-    from wechat import WeChatClient
-
-    w = WeChatClient()
-    w.template_message.send('o0fuLwfGN36wb6uLEtfmqHp1LKzU', 'RWErFfHyuTTHBMwP265x6ikNCA49CxfLITIPe7o-c4w',
-                            keyword_list=[{'value': '112'}, {'value': '111'}, {'value': '2019-7-10'}],
-                            **{'first': {'value': '你好 seven'}, 'remark': {'value': '112'}})
